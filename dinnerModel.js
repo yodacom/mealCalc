4ce984276a925e06ab1d3f9a90a698da40df5b1f
Jeremy Black <jb@yodacom.com>
2017-04-10 08:11:55 -0600

    added loop for diners

--- dinnerModel.js
+++ dinnerModel.js
@@ -1,57 +1,77 @@
-Class Dinner {
-    constructor(
-        dinnerName,
-        items[],
-        cost,
-    )
-}
 
-dinnerItems {
-    potato:{ mashed,fried, baked, hashed},
-    meat:{porkchop, steak, fish, eggs, bacon},
-    drink:{water,tea,beer,soda},
-    vegetables:{corn, greenbeans, asparagus, salad}
-    bread:[toast,loaf,rye,wheat, crackers]
-    soup:[chickenNoodle, tomato, potato]
-    other:[cheese]
-}
-var dinnersSchema = new dinnersSchema({
-    dinnerName: String,
-    dinnerItems: Array,
-    cost: Number
-})
+var dinnerItems = {
+	potato:[ "mashed","fried", "baked", "hashed"],
+	meat:["porkchop", "steak", "fish", "eggs", "bacon"],
+	drink:["water","tea","beer","soda"],
+	vegetables:["corn", "greenbeans", "asparagus", "salad"],
+	bread:["toast","loaf","rye","wheat", "crackers"],
+	soup:["chickenNoodle", "tomato", "potato"],
+	other:["cheese"]
+};
 
 var dinners = [
-    {
-    name: porkchop,
-    items: [
-        meat=meat[0],
-        potato=potato[3],
-        vegetable=null,
-        bread=[1]
-    ],
-    cost: 14.95,
-},
-    {
-    name: eggs,
-    items:[
-        meat=meat[4],
-        potato=potato[2],
-        vegetable=vegetable[3],
-        bread=[0],
-    ],
-    cost: 9.95
-    },
+	{
+		name: "porkchop",
+		items: [
+	dinnerItems.meat[0],
+	dinnerItems.potato[3],
+	dinnerItems.bread[1]
+],
+		cost: 14.95,
+	},
+	{
+		name: "eggs",
+		items:[
+	dinnerItems.bread[0],
+	dinnerItems.vegetables[3],
+	dinnerItems.other[0]
+],
+		cost: 9.95
+	},
 
-    {
-        name: soup,
-        [
-          soup:[0],
-          bread=[4],
-          other:[0]
-        ],
-    cost: 7.95
-    },
-]
+	{
+		name: "soup",
+		item:[
+	dinnerItems.meat[0],
+	dinnerItems.potato[3],
+	dinnerItems.bread[1]
+],
+		cost: 7.95
+	},
+];
+
+var diners = [
+	{name:"Bob",
+		dinner:dinners[0]    
+	},
+
+	{name:"Jane",
+		dinner:dinners[1] 
+	},
+
+    {name:"Bill",
+		dinner:dinners[1] 
+	},
+
+    {name:"Nancy",
+		dinner:dinners[1] 
+	},
+
+    {name:"Pete",
+		dinner:dinners[1] 
+	}
+
+
+
+];
+
+var totalCost = function(){
+    console.log("the dinners are:" )
+    for (i=0; i<diners.length; i++){
+	console.log(diners[i].name);
+        
+}
+};
+totalCost();
 
 
