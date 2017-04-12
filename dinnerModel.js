b96a20ede3b0251e48a805089fd3bde54d191632
Jeremy Black <jb@yodacom.com>
2017-04-12 07:52:43 -0600

    Fixed calculations to work

--- dinnerModel.js
+++ dinnerModel.js
@@ -34,7 +34,7 @@ var meals = [
 
 	{
 		name: "Soup",
-		item:[
+		items:[
 			mealItems.meat[0],
 			mealItems.potato[3],
 			mealItems.bread[1]
@@ -53,15 +53,15 @@ var diners = [
 		mealChoice:meals[1] 
 	},
 
-    {name:"Bill",
+	{name:"Bill",
 		mealChoice:meals[2] 
 	},
 
-    {name:"Nancy",
+	{name:"Nancy",
 		mealChoice:meals[0] 
 	},
 
-    {name:"Pete",
+	{name:"Pete",
 		mealChoice:meals[1] 
 	}
 
@@ -70,39 +70,30 @@ var diners = [
 ];
 
 var showDiners = function(){
-    console.log("the dinners are:" )
-    for (i=0; i<diners.length; i++){
-	console.log(diners[i].name);
-        
-}
+	console.log("the dinners are:" );
+	var total = 0;
+	for (i=0; i<diners.length; i++){
+		console.log(diners[i].name);
+		console.log(diners[i].mealChoice.name);
+		console.log(diners[i].mealChoice.cost);
+		total += diners[i].mealChoice.cost;
+		var tip = diners[i].mealChoice.cost * 0.2
+		console.log(tip);
+		var items=diners[i].mealChoice.items
+		for (j = 0; j<items.length;j++){
+			console.log(items[j]);
+		}
+	}
+	console.log(total);
+	var totalTip = total * 0.2;
+	console.log(totalTip);
+};
 
+
+showDiners();
 
 // Functions are to show list of diners, with their meal choice and items listed of that meal.
 // Then display total cost for each diner's meal'
 // Then show the total of all the meals, calculate and list total tip of 20% of total of all meals, and then divide that tip by a ratio of cost of each diners meal to total of all meals cost and display diners part of the total tip.  Then add that calculated tip to the total for each of the diners to pay. 
 
-var i;
-var text = " ";
-var dinersNames = function(){
-	console.log("the Diners are:" );
-	for (i = 0; i < diners.length; i++)
-		console.log(diners[i].name);
-	return dinersNames();
 
-	function dinerMealCost() {
-		var totalMeal;
-		for (totalMeal in meals) {
-			var list = document.getElementById("total");
-			list.innerHTML = list.innerHTML + letters[forincounter];
-		}
-	}
-};
-
-for(x in meals){
-	var temp = "<li>"+x+"<ul>";
-	for(y in meals[x]){
-		temp += "<li>"+y+":"+meals[x][y]+"</li>";
-	}
-	temp += "</ul></li>";
-	$("ul:nth-child(2)").append(temp);
-}
