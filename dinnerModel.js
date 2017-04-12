0cc91058fee0e1223032db380911c810ca7a9575
Jeremy Black <jb@yodacom.com>
2017-04-12 05:23:09 -0600

    added forIn loop and some html

--- dinnerModel.js
+++ dinnerModel.js
@@ -1,57 +1,91 @@
-Class Dinner {
-    constructor(
-        dinnerName,
-        items[],
-        cost,
-    )
+
+var mealItems = {
+	potato:[ "mashed","fried", "baked", "hashed"],
+	meat:["porkchop", "steak", "fish", "eggs", "bacon"],
+	drink:["water","tea","beer","soda","Orange Juice"],
+	vegetables:["corn", "greenbeans", "asparagus", "salad"],
+	bread:["toast","loaf","rye","wheat", "crackers"],
+	soup:["chickenNoodle", "tomato", "potato"],
+	other:["cheese"]
+};
+
+var meals = [
+	{
+		name: "Porkchop",
+		items: [
+			mealItems.meat[0],
+			mealItems.potato[3],
+			mealItems.bread[1],
+			mealItems.drink[1]
+		],
+		cost: 14.95,
+	},
+	{
+		name: "Eggs",
+		items:[
+			mealItems.bread[0],
+			mealItems.vegetables[3],
+			mealItems.other[0],
+			mealItems.drink[4]
+		],
+		cost: 9.95
+	},
+
+	{
+		name: "Soup",
+		item:[
+			mealItems.meat[0],
+			mealItems.potato[3],
+			mealItems.bread[1]
+		],
+		cost: 7.95
+	},
+];
+
+var diners = [
+	{name:"Bob",
+		mealChoice:meals[0]    
+	},
+
+	{name:"Jane",
+		mealChoice:meals[1] 
+	},
+
+	{name:"Bill",
+		mealChoice:meals[2] 
+	},
+
+	{name:"Nancy",
+		mealChoice:meals[0] 
+	},
+
+	{name:"Pete",
+		mealChoice:meals[1] 
+	}
+
+];
+var i;
+var text = " ";
+var dinersNames = function(){
+	console.log("the Diners are:" );
+	for (i = 0; i < diners.length; i++)
+		console.log(diners[i].name);
+	return dinersNames();
+
+	function mealsCost() {
+		var totalMeals;
+		for (totalMeals in meals) {
+			var list = document.getElementById("total");
+			list.innerHTML = list.innerHTML + letters[forincounter];
+		}
+	}
+};
+
+for(x in meals){
+	var temp = "<li>"+x+"<ul>";
+	for(y in meals[x]){
+		temp += "<li>"+y+":"+meals[x][y]+"</li>";
+	}
+	temp += "</ul></li>";
+	$("ul:nth-child(2)").append(temp);
 }
-
-dinnerItems {
-    potato:{ mashed,fried, baked, hashed},
-    meat:{porkchop, steak, fish, eggs, bacon},
-    drink:{water,tea,beer,soda},
-    vegetables:{corn, greenbeans, asparagus, salad}
-    bread:[toast,loaf,rye,wheat, crackers]
-    soup:[chickenNoodle, tomato, potato]
-    other:[cheese]
-}
-var dinnersSchema = new dinnersSchema({
-    dinnerName: String,
-    dinnerItems: Array,
-    cost: Number
-})
-
-var dinners = [
-    {
-    name: porkchop,
-    items: [
-        meat=meat[0],
-        potato=potato[3],
-        vegetable=null,
-        bread=[1]
-    ],
-    cost: 14.95,
-},
-    {
-    name: eggs,
-    items:[
-        meat=meat[4],
-        potato=potato[2],
-        vegetable=vegetable[3],
-        bread=[0],
-    ],
-    cost: 9.95
-    },
-
-    {
-        name: soup,
-        [
-          soup:[0],
-          bread=[4],
-          other:[0]
-        ],
-    cost: 7.95
-    },
-]
-
-
